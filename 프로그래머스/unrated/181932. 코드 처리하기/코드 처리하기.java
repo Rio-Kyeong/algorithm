a1f5e4506c0b272c668a042c8a0612a357bb78d2
Leo Kyeongwoo Ryu <drd9811@naver.com>
2023-10-16 13:35:54 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        boolean mode = false; // 0 : false, 1 : true
+        char[] codeCharacter = code.toCharArray();
+
+        for (int i = 0; i < codeCharacter.length; i++) {
+            if (codeCharacter[i] == '1') {
+                mode = !mode;
+                continue;
+            }
+            if (!mode && (i % 2 == 0)) {
+                 ret.append(codeCharacter[i]);
+            }
+            if (mode && (i % 2 != 0)) {
+                ret.append(codeCharacter[i]);
+            }
+        }
+
+        return ret.toString().equals("") ? "EMPTY" : ret.toString();
+    }
+}
