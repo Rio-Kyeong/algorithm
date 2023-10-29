147b10fc8ede562f39eb184efca793029853c08c
Leo Kyeongwoo Ryu <drd9811@naver.com>
2023-10-29 17:08:22 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 1.79 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
+
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            int minValue = Integer.MAX_VALUE;
+
+            for (int j = s; j <= e; j++) {
+                if(arr[j] > k && arr[j] < minValue) {
+                    minValue = arr[j];
+                }
+            }
+
+            if (minValue == Integer.MAX_VALUE) {
+                continue;
+            }
+            answer[i] = minValue;
+        }
+
+        return answer;
+    }
+}
