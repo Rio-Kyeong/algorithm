c4cd09763a31d4b192e7df3cade67950397d172a
Kyeongwoo Ryu <drd9811@naver.com>
2023-11-02 23:12:58 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 3.21 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -5,8 +5,8 @@ import java.util.stream.Stream;
 class Solution {
     public static int[] solution(int n) {
         return IntStream.concat(
-                Stream.iterate(n, i -> i != 1, i -> i % 2 == 0 ? i / 2 : 3 * i + 1).mapToInt(Integer::intValue)
-                , IntStream.of(1)
+                        IntStream.iterate(n, i -> i != 1, i -> i % 2 == 0 ? i / 2 : 3 * i + 1),
+                        IntStream.of(1)
         ).toArray();
     }
 }
