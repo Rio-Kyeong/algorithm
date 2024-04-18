ad2a3d9d8a9e119675f3e6d8506a282089f84734
Kyeongwoo Ryu <drd9811@naver.com>
2024-04-18 11:13:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String string) {
+        Map<String, Integer> numberMap = createMapping();
+        StringBuilder resultBuilder = new StringBuilder();
+
+        int index = 0;
+        while (string.length() > index) {
+            char charAt = string.charAt(index);
+            if (Character.isDigit(charAt)) {
+                resultBuilder.append(charAt);
+                index++;
+            } else {
+                StringBuilder wordBuilder = new StringBuilder();
+                while (string.length() > index && !Character.isDigit(charAt)) {
+                    wordBuilder.append(charAt);
+                    if (numberMap.containsKey(wordBuilder.toString())) {
+                        resultBuilder.append(numberMap.get(wordBuilder.toString()));
+                        wordBuilder.setLength(0);
+                    }
+                    index++;
+                    
+                    if (string.length() > index) {
+                        charAt = string.charAt(index);
+                    }
+                        
+                }
+            }
+        }
+
+        return Integer.parseInt(resultBuilder.toString());
+    }
+
+    private Map<String, Integer> createMapping() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+
+        return map;
+    }
+}
