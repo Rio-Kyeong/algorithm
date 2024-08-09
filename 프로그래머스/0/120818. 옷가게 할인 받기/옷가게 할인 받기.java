0081d4c0bce9153d7699ca693c5a2c569a728482
Kyeongwoo Ryu <drd9811@naver.com>
2024-08-09 14:41:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int price) {
+        double discountRate = 0.0;
+        
+        if (price >= 500000) {
+            discountRate = 0.20;
+        } else if (price >= 300000) {
+            discountRate = 0.10;
+        } else if (price >= 100000) {
+            discountRate = 0.05;
+        }
+        
+        double discountAmount = price * discountRate;
+
+        return (int) (price - discountAmount);
+    }
+}
